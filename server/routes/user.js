1e737b95e6fb068637582beec13a46b1d4fd800b
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-06-18 11:38:40 +0300

    added app

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const { Router } = require("express");
-
 const user = Router();
-
 user.use(express.json());
+
+user.get("/", (req, res) => {
+  res.send("hello");
+});
 
 module.exports = user;
