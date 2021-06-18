089afecaeb9acda05b594faaec659201d4a97ea3
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-06-18 18:43:02 +0300

    handled requests

--- server/routes/coach.js
+++ server/routes/coach.js
@@ -16,4 +16,38 @@ const { Router } = require("express");
 const coach = Router();
 coach.use(express.json());
 
+coach.put("/request/accept/:coachId", (req, res) => {
+  const { coachId } = req.params;
+  const { traineeId } = req.query;
+  if (!Number(coachId) || !Number(traineeId))
+    return res.status(400).send("Invalid ID");
+  models.Trainee.update({ coach_id: coachId }, { where: { id: traineeId } })
+    .then((data) => {
+      if (!data[0]) return res.status(404).send("No Client With That Id");
+      models.CoachRequest.destroy({
+        where: { id: traineeId, coach_id: coachId },
+      })
+        .then(() => {
+          res.status(200).send("Request Accepted");
+        })
+        .catch((err) => res.status(400).send(err.message));
+    })
+    .catch((err) => res.status(400).send(err.message));
+});
+
+coach.delete("/request/decline/:coachId", (req, res) => {
+  const { coachId } = req.params;
+  const { traineeId } = req.query;
+  if (!Number(coachId) || !Number(traineeId))
+    return res.status(400).send("Invalid ID");
+  models.CoachRequest.destroy({
+    where: { trainee_id: traineeId, coach_id: coachId },
+  })
+    .then((data) => {
+      console.log(data);
+      if (!data) return res.status(404).send("No Client With That Id");
+      res.status(200).send("Request Declined");
+    })
+    .catch((err) => res.status(400).send(err.message));
+});
 module.exports = coach;
