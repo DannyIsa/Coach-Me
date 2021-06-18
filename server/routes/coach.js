55a038af91ab323cc038d7bacaaf1d53e5d5343e
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-06-18 17:34:15 +0300

    added routes

---
+++ server/routes/coach.js
@@ -0,0 +1,19 @@
+const models = require("../models");
+const { Op } = require("sequelize");
+const Sequelize = require("sequelize");
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.SQL_DATA_BASE,
+  process.env.SQL_USERNAME,
+  process.env.SQL_PASSWORD,
+  {
+    host: process.env.SQL_HOST,
+    dialect: "mysql",
+  }
+);
+const express = require("express");
+const { Router } = require("express");
+const coach = Router();
+coach.use(express.json());
+
+module.exports = coach;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const user = require("./user");
+const coach = require("./coach");
+const trainee = require("./trainee");
 
 const api = Router();
 
 api.use("/user", user);
+api.use("/coach", coach);
+api.use("/trainee", trainee);
 
 module.exports = api;
