d84ed7241dab9e10d924a2d13b1d5bf47aae4b2f
DannyIsa <daniisakov@gmail.com>
2021-06-20 19:45:51 +0300

    diet log post api

--- server/routes/logs.js
+++ server/routes/logs.js
@@ -56,12 +56,44 @@ logs.post("/measure/add", async (req, res) => {
   });
 });
 
-logs.post("/diet/add", (req, res) => {});
+logs.post("/diet/add", (req, res) => {
+  const {
+    id: traineeId,
+    totalCalories,
+    usedCalories,
+    totalProtein,
+    usedProtein,
+    totalCarbs,
+    usedCarbs,
+    totalFat,
+    usedFat,
+  } = req.body || null;
+  const trainee = await models.Trainee.findOne({ where: { id: traineeId } });
+  if (!traineeId || !trainee) {
+    return res.status(400).send("Invalid ID");
+  }
+  if (!totalCalories || !usedCalories) {
+    return res.status(400).send("Must sand total and used caloriess");
+  }
+  models.DietLog.create({
+    id: traineeId,
+    "total-calories": totalCalories,
+    "used-calories": usedCalories,
+    "total-protein": totalProtein,
+    "used-protein": usedProtein,
+    "total-carbs": totalCarbs,
+    "used-carbs": usedCarbs,
+    "total-fat": totalFat,
+    "used-fat": usedFat,
+  }).then(() => {
+    res.status(201).send(`${traineeId} food log added`);
+  });
+});
 
-logs.get("/workout/show:traineeId", (req, res) => {});
+logs.get("/workout/show/:traineeId", (req, res) => {});
 
-logs.get("/measure/show:traineeId", (req, res) => {});
+logs.get("/measure/show/:traineeId", (req, res) => {});
 
-logs.get("/diet/show:traineeId", (req, res) => {});
+logs.get("/diet/show/:traineeId", (req, res) => {});
 
 module.exports = logs;
