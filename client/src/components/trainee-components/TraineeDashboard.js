a923d8bc3f454f96bfd1b05780a835e7ca33ed21
Moran <moranaga16@gmail.com>
2021-06-24 22:10:35 +0300

    display trainee previous workouts(has bug)

--- client/src/components/trainee-components/TraineeDashboard.js
+++ client/src/components/trainee-components/TraineeDashboard.js
@@ -2,50 +2,49 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function TraineeDashboard({ signOut, userId }) {
-  // const history = useHistory();
+function TraineeDashboard({ userDetails }) {
   const [previousWorkouts, setPreviousWorkouts] = useState([]);
   const [dietLog, setDietLog] = useState([]);
 
   const getWorkoutsLog = () =>
     axios
-      .get("http://localhost:3001/api/logs/workout/show/" + userId)
+      .get("http://localhost:3001/api/logs/workout/show/" + userDetails)
       .then(({ data }) => {
         if (data) {
-          console.log(data);
           const workouts = data.map((workout, i) => {
-            console.log(workout);
-            setPreviousWorkouts(workouts);
+            const workoutName = workout.name;
+            setPreviousWorkouts((previousWorkouts) => [
+              ...previousWorkouts,
+              workoutName,
+            ]);
           });
         }
       })
       .catch((err) => {
         console.log(err);
       });
+
+  // console.log(previousWorkouts, "after");
+  // const getdietLog = () =>
+  //   axios
+  //     .get("http://localhost:3001/api/logs/diet/show/" + userDetails)
+  //     .then(({ data }) => {
+  //       if (data) {
+  //         console.log(data);
+  //         const dietLog = data.map((log, i) => {
+  //           console.log(log);
+  //           setDietLog(dietLog);
+  //         });
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
 
   useEffect(() => {
     getWorkoutsLog();
-  }, [userId]);
-
-  const getdietLog = () =>
-    axios
-      .get("http://localhost:3001/api/logs/diet/show/" + userId)
-      .then(({ data }) => {
-        if (data) {
-          console.log(data);
-          const dietLog = data.map((log, i) => {
-            console.log(log);
-            setDietLog(dietLog);
-          });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-  useEffect(() => {
-    getdietLog();
-  }, [userId]);
+    // getdietLog();
+  }, [userDetails]);
 
   return (
     <div>
