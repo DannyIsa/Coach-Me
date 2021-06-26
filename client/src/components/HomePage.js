7a9d112359d39e50723f41d117168c4524979007
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-06-26 20:06:44 +0300

    more homepage css

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,48 +1,78 @@
-import React from "react";
-import {
-  Link,
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import { ReactComponent as CloseMenu } from "../assets/x.svg";
+import { ReactComponent as MenuIcon } from "../assets/menu.svg";
 
 import "../styles/HomePage.css";
-import SignUp from "./SignUp";
-import SignIn from "./SignIn";
-// import run from "../pics/web.mp4";
 import run from "../pics/home.mp4";
 import logo from "../pics/logo.png";
 
 function HomePage({}) {
-  return (
-    <div className="homepage">
-      <nav class="navigation">
-        <a href="#" class="navbar-logo">
-          <img src={logo} id="logo" />
-        </a>
-        <div class="navbar-right">
-          <a href="#">Home</a>
-          <a href="#">How It Works</a>
-          <a href="#">About</a>
-          <a href="#" className="login">
-            Login
-          </a>
-        </div>
-      </nav>
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
+  return (
+    <div>
+      <div className="homeNav">
+        <div className="logo-nav">
+          <div className="logo-container">
+            <a href="#">
+              <img src={logo} id="logo" />
+            </a>
+          </div>
+          <ul className={click ? "nav-options active" : "nav-options"}>
+            <li className="option" onClick={closeMobileMenu}>
+              <a href="#" className="link">
+                Home
+              </a>
+            </li>
+            <li className="option" onClick={closeMobileMenu}>
+              <a href="#" className="link">
+                How It Works
+              </a>
+            </li>
+            <li className="option" onClick={closeMobileMenu}>
+              <a href="#" className="link">
+                About
+              </a>
+            </li>
+            <li className="option mobile-option" onClick={closeMobileMenu}>
+              <a href="#" className="sign-in link">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+        <ul className="signIn">
+          <li onClick={closeMobileMenu}>
+            <Link to="/sign-in" className="signIn-btn">
+              Login
+            </Link>
+          </li>
+        </ul>
+        <div className="mobile-menu" onClick={handleClick}>
+          {click ? (
+            <CloseMenu className="menu-icon" />
+          ) : (
+            <MenuIcon className="menu-icon" />
+          )}
+        </div>
+      </div>
       <video autoPlay muted loop id="myVideo">
         <source src={run} type="video/mp4" />
       </video>
-      {/* <div id="welcome">
-        <h1>Welcome To Coach Me</h1>
-        <h2>Online coaching was never easier</h2>
-      </div> */}
-      <div className="viewport-header">
-        <h1>
-          Explore
-          <span>Montana</span>
-        </h1>
+      <div className="viewport">
+        <div className="welcome">
+          <h1 id="welcome-header"> Connect. Get fit. Enjoy. </h1>
+          <span id="welcome-span">
+            Personal training has never been so easy.
+          </span>
+          <Link to="/sign-up">
+            <button id="start">Start your change now</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
