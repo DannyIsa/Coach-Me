8b472f72e653cefb6fff9f6ed022394d91f25d7f
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-06-30 11:58:44 +0300

    added workout lists for the coach

--- client/src/components/coach-components/WorkoutsList.js
+++ client/src/components/coach-components/WorkoutsList.js
@@ -28,23 +28,36 @@ function WorkoutsList({ userDetails }) {
     <div className="create-workouts-start">
       <Link to="/coach/workouts/create">Create workout</Link>
       <h1>Your Workouts:</h1>
-      <div className="workouts-list">
-        {workouts.map((item, index) => (
-          <h1 key={"workout" + index} onClick={() => setShownWorkout(item)}>
-            {item.name}
-          </h1>
-        ))}
-      </div>
       <br />
-      {shownWorkout && shownWorkoutExercises && (
-        <div className="shown-workout">
-          <h1 className="workout-name">{shownWorkout.name}</h1>
-          {shownWorkoutExercises.map((item) => (
-            <h3 className="exercise-name">{item.name}</h3>
+      <div className="content">
+        <div className="workouts-list">
+          {workouts.map((item, index) => (
+            <h1 key={"workout" + index} onClick={() => setShownWorkout(item)}>
+              {item.name}
+            </h1>
           ))}
-          <br />
         </div>
-      )}
+        <br />
+        {shownWorkout && shownWorkoutExercises && (
+          <div className="shown-workout">
+            <h1 className="workout-name">{shownWorkout.name}</h1>
+            <ol>
+              {shownWorkoutExercises.map((item) => (
+                <li className="exercise-block">
+                  <h2 className="exercise-name">{item.name}</h2>
+                  <h3 className="exercise-details">{`${item.min_reps} ${
+                    item.min_reps !== item.max_reps ? "-" + item.max_reps : ""
+                  } reps, rest for ${item.rest}s ${
+                    item.added_weight > 0 ? "+" + item.added_weight + "kg " : ""
+                  }X${item.sets}`}</h3>
+                </li>
+              ))}
+            </ol>
+            <h1>{"X" + shownWorkout.sets}</h1>
+            <br />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
