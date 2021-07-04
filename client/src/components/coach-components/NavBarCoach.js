892b05df0effaa3389c36617c85ec8c5daee5e4b
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-07-04 14:26:53 +0300

    added notification bar

--- client/src/components/coach-components/NavBarCoach.js
+++ client/src/components/coach-components/NavBarCoach.js
@@ -1,75 +1,11 @@
-// import React, { useState } from "react";
-// import { NavLink } from "react-router-dom";
-// import "../styles/Nav.css";
-// // import { useHistory } from "react-router-dom";
-
-// function NavBarCoach({ signOut, userType }) {
-//   const [click, setClick] = useState(false);
-//   const handleClick = () => setClick(!click);
-//   // const history = useHistory();
-//   const navBarItems = [
-//     {
-//       title: "Home",
-//       link: "/",
-//       className: "nav-item",
-//     },
-//     {
-//       title: "Workouts",
-//       link: `/${userType}/workouts`,
-//       className: "nav-item",
-//     },
-//     {
-//       title: "Profile",
-//       link: "/profile",
-//       className: "nav-item",
-//     },
-//     {
-//       title: "About Us",
-//       link: "/about",
-//       className: "nav-item",
-//     },
-//   ];
-//   return (
-//     <nav className="navbar">
-//       <div className="nav-container">
-//         {/* <button className="sign-out" onClick={() => signOut(history)}>
-//           Sign Out
-//         </button> */}
-//         <NavLink exact to="/" className="nav-logo">
-//           COACH-ME
-//         </NavLink>
-//         <ul className={click ? "nav-menu active" : "nav-menu"}>
-//           {navBarItems.map((item, index) => {
-//             return (
-//               <li key={index} className={item.className}>
-//                 <NavLink
-//                   exact
-//                   to={item.link}
-//                   activeClassName="active"
-//                   className="nav-links"
-//                   onClick={handleClick}
-//                 >
-//                   {item.title}
-//                 </NavLink>
-//               </li>
-//             );
-//           })}
-//         </ul>
-//         <div className="nav-icon" onClick={handleClick}>
-//           {/* <i className={click ? "fas fa-times" : "fas fa-bars"}></i> */}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default NavBarCoach;
-
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 import { ReactComponent as CloseMenu } from "../../assets/x.svg";
 import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 import "../../styles/NavBar.css";
 import logo from "../../pics/logo.png";
@@ -95,7 +31,7 @@ function NavBarCoach({ signOut, userType }) {
             </a>
           </li>
           <li className="option" onClick={closeMobileMenu}>
-            <a href={`/${userType}/workouts`} className="link">
+            <a href={`${userType}/workouts`} className="link">
               Workouts
             </a>
           </li>
@@ -105,9 +41,167 @@ function NavBarCoach({ signOut, userType }) {
             </a>
           </li>
           <li className="option" onClick={closeMobileMenu}>
-            <a href="/coach/requests" className="link">
-              Requests
-            </a>
+            <div className="notification">
+              <a href="#">
+                <div className="notBtn" href="#">
+                  {/* // <div className="number">2</div> */}
+                  <FontAwesomeIcon
+                    icon={faBell}
+                    color="#acacac"
+                    className="fa-fa"
+                  />
+                  <div className="box">
+                    <div className="display">
+                      <div className="nothing">
+                        {/* <i className="fas fa-child stick"></i> */}
+
+                        <FontAwesomeIcon
+                          icon={faBell}
+                          color="#acacac"
+                          className="fa-fa"
+                        />
+                        <div className="cent">
+                          Looks Like your all caught up!
+                        </div>
+                      </div>
+                      <div className="cont">
+                        <div className="sec new">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="https://c1.staticflickr.com/5/4007/4626436851_5629a97f30_b.jpg"
+                              />
+                            </div>
+                            <div className="txt">
+                              James liked your post: "Pure css notification box"
+                            </div>
+                            <div className="txt sub">11/7 - 2:30 pm</div>
+                          </a>
+                        </div>
+                        <div className="sec new">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="https://obamawhitehouse.archives.gov/sites/obamawhitehouse.archives.gov/files/styles/person_medium_photo/public/person-photo/amanda_lucidon22.jpg?itok=JFPi8OFJ"
+                              />
+                            </div>
+                            <div className="txt">
+                              Annita liked your post: "Pure css notification
+                              box"
+                            </div>
+                            <div className="txt sub">11/7 - 2:13 pm</div>
+                          </a>
+                        </div>
+                        <div className="sec">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3O45RK9qyCrZJivYsY6PmeVEJH07l7bkoolJmscBsNjzump27"
+                              />
+                            </div>
+                            <div className="txt">
+                              Brie liked your post: "Pure css notification box"
+                            </div>
+                            <div className="txt sub">11/6 - 9:35 pm</div>
+                          </a>
+                        </div>
+                        <div className="sec">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="https://c1.staticflickr.com/4/3725/10214643804_75c0b6eeab_b.jpg"
+                              />
+                            </div>
+                            <div className="txt">
+                              Madison liked your post: "Pure css notification
+                              box"
+                            </div>
+                            <div className="txt sub">11/6 - 4:04 pm</div>
+                          </a>
+                        </div>
+                        <div className="sec">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/52/NG_headshot_white_shirt_square_Jan18.jpg"
+                              />
+                            </div>
+                            <div className="txt">
+                              Ted liked your post: "Pure css notification box"
+                            </div>
+                            <div className="txt sub">11/6 - 10:37 am</div>
+                          </a>
+                        </div>
+                        <div className="sec">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Pat-headshot-square.jpg"
+                              />
+                            </div>
+                            <div className="txt">
+                              Tommas liked your post: "Pure css notification
+                              box"
+                            </div>
+                            <div className="txt sub">11/5 - 7:30 pm</div>
+                          </a>
+                        </div>
+                        <div className="sec">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="https://c1.staticflickr.com/8/7407/13785133614_6254abb8c4.jpg"
+                              />
+                            </div>
+                            <div className="txt">
+                              Claire liked your post: "Pure css notification
+                              box"
+                            </div>
+                            <div className="txt sub">11/5 - 2:30 pm</div>
+                          </a>
+                        </div>
+                        <div className="sec">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="//c1.staticflickr.com/1/185/440890151_54c5b920b0_b.jpg"
+                              />
+                            </div>
+                            <div className="txt">
+                              Jerimaiah liked your post: "Pure css notification
+                              box"
+                            </div>
+                            <div className="txt sub">11/5 - 1:34 pm</div>
+                          </a>
+                        </div>
+                        <div className="sec">
+                          <a href="https://codepen.io/Golez/">
+                            <div className="profCont">
+                              <img
+                                className="profile"
+                                src="//c2.staticflickr.com/4/3397/3585544855_28442029a5_z.jpg?zz=1"
+                              />
+                            </div>
+                            <div className="txt">
+                              Debra liked your post: "Pure css notification box"
+                            </div>
+                            <div className="txt sub">11/5 - 10:20 am</div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
           </li>
           <li className="option mobile-option" onClick={closeMobileMenu}>
             <Link className="sign-in link" onClick={() => signOut(history)}>
@@ -135,3 +229,150 @@ function NavBarCoach({ signOut, userType }) {
 }
 
 export default NavBarCoach;
+
+// {/* <div className="notification">
+//   <a href="#">
+//     <div className="notBtn" href="#">
+//       {/* // <div className="number">2</div> */}
+//       <FontAwesomeIcon icon={faBell} color="#acacac" className="fa-fa" />
+//       <div className="box">
+//         <div className="display">
+//           <div className="nothing">
+//             {/* <i className="fas fa-child stick"></i> */}
+
+//             <FontAwesomeIcon icon={faBell} color="#acacac" className="fa-fa" />
+//             <div className="cent">Looks Like your all caught up!</div>
+//           </div>
+//           <div className="cont">
+//             <div className="sec new">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="https://c1.staticflickr.com/5/4007/4626436851_5629a97f30_b.jpg"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   James liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/7 - 2:30 pm</div>
+//               </a>
+//             </div>
+//             <div className="sec new">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="https://obamawhitehouse.archives.gov/sites/obamawhitehouse.archives.gov/files/styles/person_medium_photo/public/person-photo/amanda_lucidon22.jpg?itok=JFPi8OFJ"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Annita liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/7 - 2:13 pm</div>
+//               </a>
+//             </div>
+//             <div className="sec">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3O45RK9qyCrZJivYsY6PmeVEJH07l7bkoolJmscBsNjzump27"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Brie liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/6 - 9:35 pm</div>
+//               </a>
+//             </div>
+//             <div className="sec">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="https://c1.staticflickr.com/4/3725/10214643804_75c0b6eeab_b.jpg"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Madison liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/6 - 4:04 pm</div>
+//               </a>
+//             </div>
+//             <div className="sec">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="https://upload.wikimedia.org/wikipedia/commons/5/52/NG_headshot_white_shirt_square_Jan18.jpg"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Ted liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/6 - 10:37 am</div>
+//               </a>
+//             </div>
+//             <div className="sec">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Pat-headshot-square.jpg"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Tommas liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/5 - 7:30 pm</div>
+//               </a>
+//             </div>
+//             <div className="sec">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="https://c1.staticflickr.com/8/7407/13785133614_6254abb8c4.jpg"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Claire liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/5 - 2:30 pm</div>
+//               </a>
+//             </div>
+//             <div className="sec">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="//c1.staticflickr.com/1/185/440890151_54c5b920b0_b.jpg"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Jerimaiah liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/5 - 1:34 pm</div>
+//               </a>
+//             </div>
+//             <div className="sec">
+//               <a href="https://codepen.io/Golez/">
+//                 <div className="profCont">
+//                   <img
+//                     className="profile"
+//                     src="//c2.staticflickr.com/4/3397/3585544855_28442029a5_z.jpg?zz=1"
+//                   />
+//                 </div>
+//                 <div className="txt">
+//                   Debra liked your post: "Pure css notification box"
+//                 </div>
+//                 <div className="txt sub">11/5 - 10:20 am</div>
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </a>
+// </div> */}
