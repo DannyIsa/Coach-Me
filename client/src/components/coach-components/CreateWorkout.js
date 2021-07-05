35abccdd0668bd316fc5b4b7124bc83d4a55a7e4
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-07-05 04:53:25 +0300

    added style to client list page

--- client/src/components/coach-components/CreateWorkout.js
+++ client/src/components/coach-components/CreateWorkout.js
@@ -1,181 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-// import ReactDOM from "react-dom";
-// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-// // fake data generator
-// const getItems = (count, offset = 0) =>
-//   Array.from({ length: count }, (v, k) => k).map((k) => ({
-//     id: `item-${k + offset}`,
-//     content: `item ${k + offset}`,
-//   }));
-
-// // a little function to help us with reordering the result
-// const reorder = (list, startIndex, endIndex) => {
-//   const result = Array.from(list);
-//   const [removed] = result.splice(startIndex, 1);
-//   result.splice(endIndex, 0, removed);
-
-//   return result;
-// };
-
-// /**
-//  * Moves an item from one list to another list.
-//  */
-// const move = (source, destination, droppableSource, droppableDestination) => {
-//   const sourceClone = Array.from(source);
-//   const destClone = Array.from(destination);
-//   const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-//   destClone.splice(droppableDestination.index, 0, removed);
-
-//   const result = {};
-//   result[droppableSource.droppableId] = sourceClone;
-//   result[droppableDestination.droppableId] = destClone;
-
-//   return result;
-// };
-
-// const grid = 8;
-
-// const getItemStyle = (isDragging, draggableStyle) => ({
-//   // some basic styles to make the items look a bit nicer
-//   userSelect: "none",
-//   padding: grid * 2,
-//   margin: `0 0 ${grid}px 0`,
-
-//   // change background colour if dragging
-//   background: isDragging ? "lightgreen" : "grey",
-
-//   // styles we need to apply on draggables
-//   ...draggableStyle,
-// });
-
-// const getListStyle = (isDraggingOver) => ({
-//   background: isDraggingOver ? "lightblue" : "lightgrey",
-//   padding: grid,
-//   width: 250,
-// });
-
-// class CreateWorkout extends Component {
-//   state = {
-//     items: getItems(10),
-//     selected: getItems(5, 10),
-//   };
-
-//   /**
-//    * A semi-generic way to handle multiple lists. Matches
-//    * the IDs of the droppable container to the names of the
-//    * source arrays stored in the state.
-//    */
-//   id2List = {
-//     droppable: "items",
-//     droppable2: "selected",
-//   };
-
-//   getList = (id) => this.state[this.id2List[id]];
-
-//   onDragEnd = (result) => {
-//     const { source, destination } = result;
-
-//     // dropped outside the list
-//     if (!destination) {
-//       return;
-//     }
-
-//     if (source.droppableId === destination.droppableId) {
-//       const items = reorder(
-//         this.getList(source.droppableId),
-//         source.index,
-//         destination.index
-//       );
-
-//       let state = { items };
-
-//       if (source.droppableId === "droppable2") {
-//         state = { selected: items };
-//       }
-
-//       this.setState(state);
-//     } else {
-//       const result = move(
-//         this.getList(source.droppableId),
-//         this.getList(destination.droppableId),
-//         source,
-//         destination
-//       );
-
-//       this.setState({
-//         items: result.droppable,
-//         selected: result.droppable2,
-//       });
-//     }
-//   };
-
-//   // Normally you would want to split things out into separate components.
-//   // But in this example everything is just done in one place for simplicity
-//   render() {
-//     return (
-//       <DragDropContext onDragEnd={this.onDragEnd}>
-//         <Droppable droppableId="droppable">
-//           {(provided, snapshot) => (
-//             <div
-//               ref={provided.innerRef}
-//               style={getListStyle(snapshot.isDraggingOver)}
-//             >
-//               {this.state.items.map((item, index) => (
-//                 <Draggable key={item.id} draggableId={item.id} index={index}>
-//                   {(provided, snapshot) => (
-//                     <div
-//                       ref={provided.innerRef}
-//                       {...provided.draggableProps}
-//                       {...provided.dragHandleProps}
-//                       style={getItemStyle(
-//                         snapshot.isDragging,
-//                         provided.draggableProps.style
-//                       )}
-//                     >
-//                       {item.content}
-//                     </div>
-//                   )}
-//                 </Draggable>
-//               ))}
-//               {provided.placeholder}
-//             </div>
-//           )}
-//         </Droppable>
-//         <Droppable droppableId="droppable2">
-//           {(provided, snapshot) => (
-//             <div
-//               ref={provided.innerRef}
-//               style={getListStyle(snapshot.isDraggingOver)}
-//             >
-//               {this.state.selected.map((item, index) => (
-//                 <Draggable key={item.id} draggableId={item.id} index={index}>
-//                   {(provided, snapshot) => (
-//                     <div
-//                       ref={provided.innerRef}
-//                       {...provided.draggableProps}
-//                       {...provided.dragHandleProps}
-//                       style={getItemStyle(
-//                         snapshot.isDragging,
-//                         provided.draggableProps.style
-//                       )}
-//                     >
-//                       {item.content}
-//                     </div>
-//                   )}
-//                 </Draggable>
-//               ))}
-//               {provided.placeholder}
-//             </div>
-//           )}
-//         </Droppable>
-//       </DragDropContext>
-//     );
-//   }
-// }
 import WorkoutPopup from "./WorkoutPopup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
@@ -188,6 +14,25 @@ function CreateWorkout({ userDetails }) {
   const [typeTags, setTypeTags] = useState([]);
   const [muscleTags, setMuscleTags] = useState([]);
   const [popupTrigger, setPopupTrigger] = useState(false);
+
+  // const [draggedOver, setDraggedOver] = useState();
+  // const [draggedItem, setDraggedItem] = useState();
+  // const [tempNewOrder, setTempNewOrder] = useState([]);
+
+  // function changeOrder(indexToEnter, indexLocation) {
+  //   if (
+  //     (!indexToEnter && indexToEnter !== 0) ||
+  //     (!indexLocation && indexLocation !== 0) ||
+  //     indexToEnter === indexLocation
+  //   )
+  //     return;
+  //   let name = exercises[indexToEnter];
+  //   let newExercises = [...exercises].filter(
+  //     (item, index) => index !== indexToEnter
+  //   );
+  //   newExercises.splice(indexLocation, 0, name);
+  //   setTempNewOrder(newExercises);
+  // }
 
   function addItem(array, str) {
     if (str.includes(",")) {
@@ -284,7 +129,21 @@ function CreateWorkout({ userDetails }) {
         <div className="new-build-workout">
           <h1>New Workout :</h1>
           {chosen.map((item, index) => (
-            <div className="chosen-exercise" key={"chosen" + index}>
+            <div
+              className="chosen-exercise"
+              key={"chosen" + index}
+              // onDragEnd={() => {
+              //   setDraggedItem(undefined);
+              //   setExercises(tempNewOrder);
+              // }}
+              // onDragEnter={() => {
+              //   setDraggedOver(index);
+              // }}
+              // onDragLeave={() => {
+              //   changeOrder(draggedItem, draggedOver);
+              //   setDraggedOver(undefined);
+              // }}
+            >
               {item}
               <button
                 onClick={() => {
@@ -312,7 +171,14 @@ function CreateWorkout({ userDetails }) {
       <div className="exercises-list">
         {exercises.length > 0
           ? exercises.map((item, index) => (
-              <div className="exercise-block" key={"exerciseItem" + index}>
+              <div
+                className="exercise-block"
+                key={"exerciseItem" + index}
+                // draggable
+                // onDragStart={() => {
+                //   setDraggedItem(index);
+                // }}
+              >
                 <h2 className="exercise-name">{item.name}</h2>
                 <img
                   className="exercise-image"
