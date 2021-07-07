0daa11f50790b1ad1781c44c8681c176f51146ba
DannyIsa <daniisakov@gmail.com>
2021-07-07 20:53:38 +0300

    d

--- client/src/components/routers/TraineeRouter.js
+++ client/src/components/routers/TraineeRouter.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CoachesList from "../trainee-components/CoachesList";
 import WeeklyCalendar from "../trainee-components/WeeklyCalendar";
 import WorkoutTimer from "../trainee-components/WorkoutTimer";
 import TraineeProfile from "../trainee-components/TraineeProfile";
@@ -13,9 +12,7 @@ function TraineeRouter({ userDetails, alertMessage }) {
         <Route exact path="/trainee/profile">
           <TraineeProfile userDetails={userDetails} />
         </Route>
-        <Route exact path="/trainee/coaches">
-          <CoachesList userDetails={userDetails} alertMessage={alertMessage} />
-        </Route>
+
         <Route exact path="/trainee/calendar">
           <WeeklyCalendar userDetails={userDetails} />
         </Route>
