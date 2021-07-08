b063fc15a301256ec62cd2e9064b353590610630
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-07-08 14:48:04 +0300

    logged in the the aws cloud

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.SQL_USERNAME,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATA_BASE,
-    host: process.env.SQL_HOST,
+    username: process.env.SQL_USERNAME_PRODUCTION,
+    password: process.env.SQL_PASSWORD_PRODUCTION,
+    database: process.env.SQL_DATA_BASE_PRODUCTION,
+    host: process.env.SQL_HOST_PRODUCTION,
     dialect: "mysql",
   },
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`, process.env.NODE_ENV);
 });
